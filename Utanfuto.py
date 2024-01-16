20e81f69b3561195b1eabdbe3d3b0eb8601ca2e1
misiu9119 <misiu9119@gmail.com>
2024-01-16 18:16:50 +0100

    Initial commit

---
+++ Utanfuto.py
@@ -0,0 +1,147 @@
+"""Név: Urbán Mihály
+Szak: Mérnökinformatika Szoftverfejlesztő szakirány
+Neptun azonosító: YQV5BS
+
+
+1. Miért hasznosak a design pattern-ök a szoftverfejlesztésben?
+a) Megnövelik a kód futási sebességét
+b) Megkönnyítik az összetett rendszerek dokumentálását
+c) Javítják a kód újrafelhasználhatóságát és karbantarthatóságát
+d) Csökkentik a szoftver licencköltségeit
+Válasz: c
+
+2. Mire szolgál az init metódus Pythonban?
+a) Egy osztály törlésére
+b) Az osztály statikus metódusainak definiálására
+c) Egy osztály metódusainak futtatására
+d) Egy osztály példányának inicializálására
+Válasz: d
+
+3. Mi a célja az egységbezárásnak (encapsulation) az OOP-ban?
+a) Az adatok és metódusok elrejtése a külvilág elől
+b) Objektumok közötti kommunikáció javítása
+c) Az öröklődés megkönnyítése
+d) A program futási sebességének növelése
+Válasz: a
+
+4. Mi a helyes módja egy osztálypéldány attribútumának elérésének Pythonban?
+a) Class.attribute
+b) Class->attribute
+c) Class::attribute
+d) instance.attribute
+Válasz: d
+
+5. Mi a különbség az attribútum és a metódus között egy osztályban?
+a) Az attribútumok értékeket tárolnak, a metódusok pedig műveleteket végeznek
+b) Az attribútumok konstruktorok, a metódusok pedig destruktorok
+c) Az attribútumok csak osztályváltozók, a metódusok pedig példányváltozók
+d) Nincs különbség, mindkettő ugyanazt a célt szolgálja
+Válasz: a
+
+6. Hogyan hozunk létre egy új osztálypéldányt Pythonban?
+a) ClassName()
+b) ClassName.new()
+c) new ClassName()
+d) ClassName.create()
+Válasz: a
+
+7. Mi a célja a Singleton patternnek?
+a) Több példány létrehozása
+b) Különböző típusú objektumok létrehozása
+c) Az objektumok többszörös öröklődése
+d) Egyetlen globálisan elérhető példány biztosítása
+Válasz: d
+
+8. Melyik Python kulcsszót használjuk az öröklődés megvalósításához?
+a) class Child(Parent):
+b) super
+c) inherits
+d) extends
+Válasz: a
+
+9. Mi a "class" kulcsszó szerepe Pythonban?
+a) Osztály létrehozása
+b) Fájl kezelés
+c) Modul importálás
+d) Függvény definiálás
+Válasz: a
+
+10. Mi a célja az Adapter design patternnek?
+a) Objektumok gyorsabb létrehozása
+b) Két inkompatibilis interfész összekapcsolása
+c) Kód ismétlésének csökkentése
+d) Programozási hibák automatikus javítása
+Válasz: b
+
+11. Milyen típusú a Factory design pattern?
+a) Létrehozási
+b) Szerkezeti
+c) Viselkedési
+d) Iteratív
+Válasz: a
+
+12. Miért használunk öröklődést (inheritance) az OOP-ban?
+a) Az osztályok egyszerűbbé válnak
+b) Kód újrafelhasználhatósága és kódmegosztás
+c) Biztonsági okokból
+d) Adatbázis optimalizálás
+Válasz: b
+
+13. Mi történik Pythonban, ha egy osztályban nem definiáljuk a init metódust?
+a) A program hibát generál
+b) Az osztály nem használható
+c) Az osztály automatikusan kap egy alapértelmezett init metódust
+d) Az osztály minden metódusa hibás lesz
+Válasz: c
+
+14. Mi a Singleton pattern alapvető tulajdonsága?
+a) Több példány létrehozása
+b) Különböző típusú objektumok létrehozása
+c) Az objektumok többszörös öröklődése
+d) Egyetlen globálisan elérhető példány biztosítása
+Válasz: d
+
+15. Melyik NEM egy Python beépített kivételkezelő szerkezet?
+a) try...except
+b) try...finally
+c) try...catch
+d) try...except...finally
+Válasz: c
+
+16. Mi a célja a Factory design patternnek?
+a) Létrehozási
+b) Szerkezeti
+c) Viselkedési
+d) Iteratív
+Válasz: a
+
+17. Milyen kijelentés igaz az absztrakt osztályokra Pythonban?
+a) Absztrakt osztályokat lehet példányosítani
+b) Absztrakt osztályokban minden metódusnak absztraktnak kell lennie
+c) Absztrakt osztályokat nem lehet örökíteni
+d) Absztrakt osztályok absztrakt metódusokat tartalmaznak, amelyeket a leszármazottaknak kell implementálniuk
+Válasz: d
+
+18. Mi az OOP célja az alábbiakból?
+a) Adatbázis kezelés
+b) Alacsony szintű memóriakezelés
+c) Eljárási, procedurális programozás kiegészítése
+d) Grafikus felhasználói felület kialakítása
+Válasz: c
+
+19. Mi a célja az Adapter design patternnek?
+a) Objektumok gyorsabb létrehozása
+b) Két inkompatibilis interfész összekapcsolása
+c) Kód ismétlésének csökkentése
+d) Programozási hibák automatikus javítása
+Válasz: b
+
+20. Mi a különbség az attribútum és a metódus között egy osztályban?
+a) Az attribútumok értékeket tárolnak, a metódusok pedig műveleteket végeznek
+b) Az attribútumok konstruktorok, a metódusok pedig destruktorok
+c) Az attribútumok csak osztályváltozók, a metódusok pedig példányváltozók
+d) Nincs különbség, mindkettő ugyanazt a célt szolgálja
+Válasz: a
+
+"""
+
